8848665b4182ebbb13f1cd8aecbe046e869e097b
jukialen <57296419+jukialen@users.noreply.github.com>
2022-01-10 18:50:07 +0100

    Changing a part of colours for home page and scrollbar  hover colour in Aside

--- components/atoms/Photos/Photos.tsx
+++ components/atoms/Photos/Photos.tsx
@@ -1,5 +1,10 @@
 import styles from './Photos.module.scss';
+import { Image } from 'antd';
 
-export const Photos = (...props: any[]) => {
-  return <div className={styles.photos} {...props} />;
+export const Photos = (link: string, alternativeText: any, ...props: any[]) => {
+  return <div className={styles.photos} {...props}>
+    {/*<Image src={link} layout='fill' alt={alternativeText} aria-label={alternativeText} priority />*/}
+  </div>;
 };
+
+// <html>TS2322: Type '{ src: string; layout: string; alt: string; &quot;aria-label&quot;: string; priority: true; }' is not assignable to type 'IntrinsicAttributes &amp; ImageProps &amp; { children?: ReactNode; }'.<br/>Property 'layout' does not exist on type 'IntrinsicAttributes &amp; ImageProps &amp; { children?: ReactNode; }'.
--- components/organisms/GalleryAccount/GalleryAccount.tsx
+++ components/organisms/GalleryAccount/GalleryAccount.tsx
@@ -1,10 +1,71 @@
+import { ref, getDownloadURL, listAll } from 'firebase/storage';
+import { auth, storage } from '../../../firebase';
 
 import { Photos } from 'components/atoms/Photos/Photos';
 import { FilesUpload } from 'components/molecules/FilesUpload/FilesUpload';
 
 import styles from './GalleryAccount.module.scss';
+import { useEffect, useState } from 'react';
 
 export const GalleryAccount = ({ data }: any ) => {
+  const user = auth.currentUser;
+  
+  const [ userPhotos, setUserPhotos ] = useState('');
+  
+  const userFilesRef = ref(storage, `${user?.uid}`);
+  
+  // useEffect(() => {
+  //   let images: string[] = [];
+  //   listAll(userFilesRef)
+  //   .then( async (res) => {
+  //     await res.prefixes.forEach((folderRef) => {
+  //       console.log(folderRef);
+  //     });
+  //     await res.items.forEach( (itemRef) => {
+  //       // All the items under listRef.
+  //       console.log(itemRef);
+  //       console.log(itemRef.fullPath);
+  //       const photoUrl = getDownloadURL(itemRef);
+  //       images.push(photoUrl);
+  //       setUserPhotos(images);
+  //       console.log(userPhotos[0])
+  //       console.log(userPhotos)
+  //
+  //     });
+  //   }).catch((error) => {
+  //     console.log(error);
+  //     // Uh-oh, an error occurred!
+  //   });
+  // }, [])
+  
+  useEffect( async() => {
+      let images: string[] = [];
+  
+    try {
+      const res = listAll(userFilesRef);
+      console.log('res', res)
+      await res.prefixes.forEach((folderRef) => {
+        console.log(folderRef);
+      });
+      // console.log(res.PromiseAll)
+      await res.items.forEach(async(itemRef) => {
+        // All the items under listRef.
+        console.log(itemRef);
+        console.log(itemRef.fullPath);
+        const photoUrl = await getDownloadURL(itemRef);
+        await images.push(photoUrl);
+        setUserPhotos(images);
+        console.log(userPhotos[0])
+        console.log(userPhotos)
+    
+      });
+    } catch (e) {
+    
+    }
+  }, [])
+  
+  // listAll(userFilesRef)
+  
   return (
     <article id='user__gallery__in__account' className={styles.user__gallery__in__account}>
       <em className={styles.title}>{data?.Account?.gallery?.userFilesTitle}</em>
@@ -12,23 +73,23 @@ export const GalleryAccount = ({ data }: any ) => {
       <FilesUpload />
     
       <div className={styles.user__photos}>
+        {/*{userPhotos.forEach(e => <Photos alternativeText={e} link={e} />)}*/}
         <Photos />
         <Photos />
         <Photos />
-        <Photos />
-        <Photos />
-        <Photos />
+        {/*<Photos />*/}
+        {/*<Photos />*/}
       </div>
       
       <em className={styles.title}>{data?.Account?.gallery?.userLikedFiles}</em>
       
       <div className={styles.like__photos}>
-        <Photos />
-        <Photos />
-        <Photos />
-        <Photos />
-        <Photos />
-        <Photos />
+        {/*<Photos />*/}
+        {/*<Photos />*/}
+        {/*<Photos />*/}
+        {/*<Photos />*/}
+        {/*<Photos />*/}
+        {/*<Photos />*/}
       </div>
     </article>
   );
