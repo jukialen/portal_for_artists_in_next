bba9fce191d8a04c63c8d869f5ddfe4eac8340ae
jukialen <57296419+jukialen@users.noreply.github.com>
2022-01-21 17:30:47 +0100

    Deleting: api folder

--- pages/api/constants/client.js
+++
@@ -1,7 +0,0 @@
-import { GraphQLClient } from 'graphql-request';
-
-export const client = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT, {
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN}`,
-  },
-});
