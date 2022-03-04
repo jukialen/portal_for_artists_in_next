9031eaf98fef35c060f90b14fb5cff0c93e635ab
jukialen <57296419+jukialen@users.noreply.github.com>
2022-03-04 20:08:30 +0100

    Adding: deletion account for a user.

--- pages/account/[displayName].tsx
+++ pages/account/[displayName].tsx
@@ -2,6 +2,7 @@ import { auth } from '../../firebase';
 
 import { useHookSWR } from 'hooks/useHookSWR';
 import { useCurrentUser } from 'hooks/useCurrentUser';
+import { useUserData } from 'hooks/useUserData';
 
 import { HeadCom } from 'components/atoms/HeadCom/HeadCom';
 import { AccountMenu } from 'components/molecules/AccountMenu/AccountMenu';
@@ -10,20 +11,20 @@ import { FilesUpload } from 'components/molecules/FilesUpload/FilesUpload';
 import { PhotosGallery } from 'components/organisms/PhotosGallery/PhotosGallery';
 import { VideoGallery } from 'components/organisms/VideoGallery/VideoGallery';
 import { ProfileAccount } from 'components/organisms/ProfileAccount/ProfileAccount';
+import { AnimatedGallery } from 'components/organisms/AnimatedGallery/AnimatedGallery';
 
 import styles from './index.module.scss';
-import { AnimatedGallery } from '../../components/organisms/AnimatedGallery/AnimatedGallery';
-import { useUserData } from '../../hooks/useUserData';
+import { DeleteAccount } from '../../components/atoms/DeleteAccount/DeleteAccount';
 
 export default function Account() {
-  const currentUser = auth.currentUser;
-  
+  const user = auth.currentUser;
   const data = useHookSWR();
   const loading = useCurrentUser('/');
   const { pseudonym } = useUserData();
+  
   return !loading ? (
     <section className='workspace'>
-      <HeadCom path={`/account/${pseudonym || currentUser?.displayName}`} content='Account portal site.' />
+      <HeadCom path={`/account/${pseudonym || user?.displayName}`} content='Account portal site.' />
       
       <h2 className={styles.account__h2}>{data?.Nav?.account}</h2>
       
@@ -40,6 +41,8 @@ export default function Account() {
       <VideoGallery data={data} />
       
       <ProfileAccount data={data} />
+      
+      <DeleteAccount />
     </section>
   ) : null
 };
