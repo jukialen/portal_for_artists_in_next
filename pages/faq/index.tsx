e5fed6eda1aef63861554ccd4413a6c7dbbd0de6
jukialen <57296419+jukialen@users.noreply.github.com>
2022-03-04 18:42:05 +0100

    Update Faq subpage

--- pages/faq/index.tsx
+++ pages/faq/index.tsx
@@ -1,16 +1,18 @@
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { Collapse } from 'antd';
+import { useRouter } from 'next/router';
 
-import { useHookSWR } from 'hooks/useHookSWR';
+import { HeadCom } from 'components/atoms/HeadCom/HeadCom';
+import { FaqItems } from 'components/atoms/FaqItems/FaqItems';
 
 import styles from './index.module.scss';
-import { HeadCom } from '../../components/atoms/HeadCom/HeadCom';
+import { Accordion, Link } from '@chakra-ui/react';
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 export default function Faq() {
-  const { Panel } = Collapse;
   
-  const { locale, asPath } = useRouter();
+  const { asPath } = useRouter();
+  const backgroundColor = '#FFD068';
+  const color = '#333';
+  const m = '2rem 0';
   
   return (
     <section className='workspace'>
@@ -18,29 +20,44 @@ export default function Faq() {
       
       <h2 className={styles.h2}>Częste pytania</h2>
       
-      <Collapse className={styles.collapse} destroyInactivePanel={true}>
-        <Panel header='Czy korzystanie z serwisu jest darmowe?' className='questions__faq' key='1'>
-          <p className={styles.answers__faq}>
-            Tak. Z serwisu można korzystać za darmo. Jednak aby skorzystać z dodatkowych korzyści,
-            należy wykupić plan Premium.
-          </p>
-        </Panel>
-        <Panel header='Jakie korzyści daje plan Premium?' className={styles.questions__faq} key='2'>
-          <p className={styles.answers__faq}>
-            Plan Premium m. in. zapewnia priorytetowe wsparcie naszego wsparcia klienta oraz brak
-            reklam. Jeśli chcesz się więcej dowiedzieć, możesz dowiedzieć się <a href='#'>tutaj</a>.
-          </p>
-        </Panel>
-        <Panel
-          header='Czy wykupienie usługi Premium jest potrzebne do korzystania z serwisu?'
-          key='3'
-          className={styles.questions__faq}
-        >
-          <p className={styles.answers__faq}>
-            Nie jest potrzebne. Możesz korzystać z serwisu bez planu Premium.
-          </p>
-        </Panel>
-      </Collapse>
+      <Accordion defaultIndex={[0]} allowMultiple width='24rem' maxW='70vw' alignSelf='center' m='2rem 0'>
+        
+        <FaqItems
+          textHead='Czy korzystanie z serwisu jest darmowe?'
+          textBody='Tak. Z serwisu można korzystać za darmo. Jednak aby skorzystać z dodatkowych korzyści,
+            należy wykupić plan Premium.'
+          bcc={backgroundColor}
+          c={color}
+          m={m}
+        />
+        
+        <FaqItems
+          textHead='Jakie korzyści daje plan Premium?'
+          textBody={<div>Plan Premium m. in. zapewnia priorytetowe wsparcie naszego wsparcia klienta oraz brak
+            reklam. Jeśli chcesz się więcej dowiedzieć, możesz dowiedzieć się <Link href='#' color='#4F8DFF' isExternal>
+              tutaj <ExternalLinkIcon mx='2px' color='#4F8DFF' /></Link>.</div>}
+          bcc={backgroundColor}
+          c={color}
+          m={m}
+        />
+        
+        <FaqItems
+          textHead='Czy wykupienie usługi Premium jest potrzebne do korzystania z serwisu?'
+          textBody='Nie jest potrzebne. Możesz korzystać z serwisu bez planu Premium.'
+          bcc={backgroundColor}
+          c={color}
+          m={m}
+        />
+        
+        <FaqItems
+          textHead='Jak mogę usunąć konto?'
+          textBody='Wystarczy, żę klikniesz w przycisk na dole strony swojego konta i zatwierdzisz wybór. Po zatwierdzeniu
+            rozpoczyna się usuwanie wszystkich twoich danych.'
+          bcc={backgroundColor}
+          c={color}
+          m={m}
+        />
+      </Accordion>
     </section>
   );
 };
