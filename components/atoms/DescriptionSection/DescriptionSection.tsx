446e240aadd156b3f4f4e5f31ea314b896898fd2
jukialen <piteru1910@yahoo.com>
2022-07-24 14:56:58 +0200

    Adding and display group regulations

--- components/atoms/DescriptionSection/DescriptionSection.tsx
+++ components/atoms/DescriptionSection/DescriptionSection.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { auth } from '../../../firebase';
-import { updateDoc } from 'firebase/firestore';
+import { getDoc, setDoc, updateDoc } from 'firebase/firestore';
 import { ErrorMessage, Form, Formik } from 'formik';
 import * as Yup from 'yup';
-import { Button, Divider, Textarea, IconButton } from '@chakra-ui/react';
+import { Button, Divider, IconButton, Textarea } from '@chakra-ui/react';
 
-import { FormType} from 'types/global.types';
+import { FormType } from 'types/global.types';
 
 import { usersInGroup } from 'references/referencesFirebase';
 
@@ -25,36 +25,71 @@ type NewDescType = {
   newDescription: string;
 };
 
+type NewRegulationType = {
+  newRegulation: string
+}
+
 export const DescriptionSection = ({ description, admin, name }: DescriptionSectionType) => {
-  const data = useHookSWR();
+  const [regulation, setRegulation] = useState<NewRegulationType[]>([]);
   const [openForm, setOpenForm] = useState(false);
+  const [openUpRegulations, setOpenUpRegulations] = useState(false);
   
+  const data = useHookSWR();
   const currentUser = auth.currentUser?.uid;
   
-  const initialValues = {
-    newDescription: description
-  };
+  const initialValuesDes = { newDescription: description };
   
-  const schemaNew = Yup.object({
-    newDescription: SchemaValidation().description,
+  const schemaNewDes = Yup.object({
+    newDescription: SchemaValidation().description
   });
   
-  const updateDescription = async ({ newDescription }: NewDescType, { resetForm }: FormType) => {
+  const getRegulation = async () => {
     try {
-      await updateDoc(usersInGroup(name!), { description: newDescription });
-      resetForm(initialValues);
+      const docSnap = await getDoc(usersInGroup(name!));
+      
+      docSnap.exists() ? setRegulation(docSnap.data().regulation) : console.log('No regulation!');
     } catch (e) {
       console.error(e);
     }
   };
   
-  return <section className={styles.container__description}>
-    <h2 className={styles.description__title}>{data?.AnotherForm?.description}</h2>
+  useEffect(() => {
+    !!name && getRegulation();
+  }, [name]);
+  
+  const updateDescription = async ({ newDescription }: NewDescType, { resetForm }: FormType) => {
+    try {
+      await updateDoc(usersInGroup(name!), { description: newDescription });
+      resetForm(initialValuesDes);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  
+  const initialValuesReg = { newRegulation: !!regulation && regulation.length > 0 ? regulation.join('\n') : data?.Regulations?.noRegulation };
+  
+  const schemaNewReg = Yup.object({
+    newRegulation: SchemaValidation().description
+  });
+  
+  const updateRegulations = async ({ newRegulation }: NewRegulationType, { resetForm }: FormType) => {
+    try {
+      const newRerArray = newRegulation.split(/\r?\n/).filter(el => el);
+      await setDoc(usersInGroup(name!), { regulation: newRerArray }, { merge: true });
+      resetForm(initialValuesReg);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  
+  return <section className={styles.container}>
+    <h2 className={styles.title}>{data?.AnotherForm?.description}</h2>
+    
     <Divider />
-    <div className={styles.desField}>
-      {!openForm ? <p className={styles.description}>{description}</p> : <Formik
-        initialValues={initialValues}
-        validationSchema={schemaNew}
+    <div className={styles.field}>
+      {!openForm ? <p className={styles.items}>{description}</p> : <Formik
+        initialValues={initialValuesDes}
+        validationSchema={schemaNewDes}
         onSubmit={updateDescription}
       >
         {({ values, handleChange }) => (
@@ -71,6 +106,55 @@ export const DescriptionSection = ({ description, admin, name }: DescriptionSect
             />
       
             <p><ErrorMessage name='newDescription' /></p>
+      
+            <Button
+              type='submit'
+              colorScheme='blue'
+              className={styles.addingButton}
+            >
+              {data?.Description?.submit}
+            </Button>
+          </Form>
+        )}
+      </Formik>}
+      {
+        admin === currentUser &&
+        <IconButton
+          icon={<EditIcon />}
+          className={styles.changeButton}
+          aria-label={data?.Description?.iconButton}
+          onClick={() => setOpenForm(!openForm)}
+        />
+      }
+    </div>
+    
+    <h2 className={styles.title}>{data?.Regulations?.regulation}</h2>
+    <Divider />
+    <div className={styles.field}>
+      {!openUpRegulations ? <div className={styles.items}>
+        {!!regulation && regulation.length > 0 ? regulation.map((reg, index) => <p
+          key={index}
+          className={styles.regulations__item}
+        >{reg}</p>) : <p className={styles.regulations__no__item}>{data?.Regulations?.noRegulation}</p>}
+      </div> : <Formik
+        initialValues={initialValuesReg}
+        validationSchema={schemaNewReg}
+        onSubmit={updateRegulations}
+      >
+        {({ values, handleChange }) => (
+          <Form className={styles.form}>
+            <Textarea
+              id='newRegulation'
+              name='newRegulation'
+              value={values.newRegulation}
+              onChange={handleChange}
+              resize='vertical'
+              placeholder={data?.Description?.textPlaceholder}
+              aria-label={data?.Description?.textAria}
+              className={styles.formDescription}
+            />
+            
+            <p><ErrorMessage name='newRegulation' /></p>
             
             <Button
               type='submit'
@@ -82,55 +166,15 @@ export const DescriptionSection = ({ description, admin, name }: DescriptionSect
           </Form>
         )}
       </Formik>}
-      {admin === currentUser &&
+      {
+        admin === currentUser &&
         <IconButton
+          icon={<EditIcon />}
+          className={styles.changeButton}
           aria-label={data?.Description?.iconButton}
-          onClick={() => setOpenForm(!openForm)}
-          icon={<EditIcon fontSize='1.2rem' />} />
+          onClick={() => setOpenUpRegulations(!openUpRegulations)}
+        />
       }
     </div>
-   
-    <h2 className={styles.description}>{data?.Regulations}</h2>
-    <Divider />
-    <p className={styles.regulations__item}>
-      1) Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    </p>
-    <p className={styles.regulations__item}>
-      2) Pellentesque scelerisque tortor nec ex mattis, non consectetur sapien imperdiet. Cras sed sem
-      volutpat arcu gravida mattis non commodo ligula. Curabitur sed magna in nisi rutrum iaculis. Morbi sed
-      nulla et odio finibus viverra a ac leo. Quisque a enim pharetra, cursus est ut, ullamcorper nibh.
-    </p>
-    <p className={styles.regulations__item}>
-      3) Nam auctor sem eu ipsum consequat rutrum. Phasellus vel nulla sodales, finibus felis nec, ullamcorper
-      metus.
-    </p>
-    <p className={styles.regulations__item}>
-      4) Donec ac magna ac risus egestas semper eget vel purus. Curabitur luctus sem sed maximus tincidunt.
-      Nullam efficitur leo quis pretium efficitur. Ut id ex eu odio tristique elementum.
-    </p>
-    <p className={styles.regulations__item}>
-      5) Aliquam non nisi ac nulla commodo porta. Morbi id metus eget arcu dictum sodales eu at sapien.
-    </p>
-    <p className={styles.regulations__item}>
-      6) Praesent quis justo non dolor tincidunt convallis pellentesque sit amet libero. Sed sit amet tortor
-      tempus, viverra ex et, vestibulum justo. Pellentesque quis leo sed purus accumsan commodo. Duis varius
-      lorem at justo rhoncus finibus. Phasellus in tellus a lacus accumsan blandit condimentum sed dolor.
-    </p>
-    <p className={styles.regulations__item}>
-      7) Etiam id nulla a mi molestie ultrices et et neque. Quisque id ligula nec felis facilisis imperdiet.
-    </p>
-    <p className={styles.regulations__item}>
-      8) Curabitur mattis dolor at pellentesque lobortis. Morbi vestibulum ante tincidunt, commodo metus non,
-      placerat tortor. Duis mattis urna ut felis ultrices rhoncus.
-    </p>
-    <p className={styles.regulations__item}>
-      9) Integer vel orci rutrum, malesuada risus non, lobortis est. Duis eleifend dui in quam commodo egestas.
-    </p>
-    <p className={styles.regulations__item}>
-      10) Praesent vehicula nisl id mauris lobortis imperdiet. In facilisis neque a libero molestie, sed
-      lacinia urna aliquam. Aenean sed dolor porta, venenatis dolor et, sollicitudin ante. Etiam at nisl eget
-      libero lobortis faucibus.
-    </p>
   </section>
-  
 }
