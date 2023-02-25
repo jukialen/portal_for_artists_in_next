24f43d996237ac25b6e443f8505279a3bf730fbe
jukialen <pbidny@gmail.com>
2023-02-25 20:51:32 +0100

    Addiction EmailVerification function from superTokens

--- config/superTokens.tsx
+++ config/superTokens.tsx
@@ -1,6 +1,7 @@
 import SuperTokens from 'supertokens-web-js';
 import Session from 'supertokens-web-js/recipe/session';
 import ThirdPartyEmailPassword from 'supertokens-web-js/recipe/thirdpartyemailpassword'
+import EmailVerification from "supertokens-web-js/recipe/emailverification";
 
 SuperTokens.init({
     appInfo: {
@@ -10,6 +11,7 @@ SuperTokens.init({
     },
     recipeList: [
       ThirdPartyEmailPassword.init(),
+      EmailVerification.init(),
       Session.init(),
     ],
 
