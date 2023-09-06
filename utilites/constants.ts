de63cfd713b3a0635afda57a2340950aa12e09d1
jukialen <pbidny@gmail.com>
2023-09-06 13:48:12 +0200

    Deletion of unused imports from constants.ts

--- utilites/constants.ts
+++ utilites/constants.ts
@@ -1,5 +1,2 @@
-import { DateObjectType } from 'types/global.types';
-import { useHookSWR } from 'hooks/useHookSWR';
-
 export const cloudFrontUrl = `${process.env.NEXT_PUBLIC_S3_URL}`;
 export const backUrl = `${process.env.NEXT_PUBLIC_BACK_URL}`;
