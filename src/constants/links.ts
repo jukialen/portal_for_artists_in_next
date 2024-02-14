f91f661504a9d4ef8b05217521ed862b5a6ed1c0
jukialen <pbidny@gmail.com>
2024-02-14 13:02:02 +0100

    Update links.ts

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,4 +1,6 @@
 export const cloudFrontUrl = `${process.env.NEXT_PUBLIC_S3_URL}`;
 export const backUrl = `${process.env.NEXT_PUBLIC_BACK_URL}`;
 
+export const GTM_ID = process.env.NEXT_PUBLIC_G_TAG;
+
 export const darkMode = 'dark';
