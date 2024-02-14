ee3b2cf5be8870af714690ce82983a91310662da
jukialen <pbidny@gmail.com>
2024-02-14 13:05:13 +0100

    Update middleware.ts

--- src/constants/middleware.ts
+++ src/constants/middleware.ts
@@ -1,11 +1,16 @@
 import { NextRequest } from 'next/server'
 import { createI18nMiddleware } from 'next-international/middleware'
+import * as process from "process";
 
 const i18n = createI18nMiddleware({
-  locales: ['en', 'pl', 'jp'],
-  defaultLocale: 'en',
-  urlMappingStrategy: 'rewrite'
-})
+    locales: ['en', 'pl', 'jp'],
+    defaultLocale: 'en',
+    urlMappingStrategy: 'rewriteDefault',
+    resolveLocaleFromRequest: request => {
+      return 'en'
+    },
+  }
+)
 
 export function middleware(request: NextRequest) {
   return i18n(request)
