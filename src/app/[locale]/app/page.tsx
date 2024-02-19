6c3f54a54fd418fcd8adf907592c363cc8d851fd
jukialen <pbidny@gmail.com>
2024-02-19 19:05:33 +0100

    Updatiion /app

--- src/app/[locale]/app/page.tsx
+++ src/app/[locale]/app/page.tsx
@@ -1,52 +1,51 @@
 import { Metadata } from 'next';
+import { cookies } from 'next/headers';
 import { setStaticParamsLocale } from 'next-international/server';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 
 import { HeadCom } from 'constants/HeadCom';
-import { backUrl, cloudFrontUrl } from 'constants/links';
+import { cloudFrontUrl } from 'constants/links';
+import { selectFiles } from 'constants/selects';
+import { Database } from 'types/database.types';
+import { DateObjectType, FileType, LangType } from 'types/global.types';
 
 import { getScopedI18n } from 'locales/server';
 
-import { FileType } from 'types/global.types';
-
 import { getDate } from 'helpers/getDate';
-import { get } from 'helpers/methods';
-
 import { dateData } from 'helpers/dateData';
 
 import { AppWrapper } from 'components/atoms/AppWrapper/AppWrapper';
 import { ZeroFiles } from 'components/atoms/ZeroFiles/ZeroFiles';
 import { AppTop10s } from 'components/molecules/AppTop10s/AppTop10s';
-import { SkeletonRootLayout } from 'components/organisms/SkeletonRootLayout/SkeletonRootLayout';
 
 import styles from './page.module.scss';
 
-type DataDateObjectType = { second: string; minute: string; hour: string; day: string; yearDateSeparator: string };
-
 export const metadata: Metadata = HeadCom('Main site for logged in users.');
 
-async function getTop10Drawings(maxItems: string, locale: string, dataDateObject: DataDateObjectType) {
+const supabase = createServerComponentClient<Database>({ cookies });
+
+async function getTop10Drawings(maxItems: number, locale: LangType, dataDateObject: DateObjectType) {
   try {
     const filesArray: FileType[] = [];
 
-    const drawings = await get(`${backUrl}/files/all`, {
-      queryData: {
-        where: {
-          AND: [{ tags: 'realistic' }, { tags: 'manga' }, { tags: 'anime' }, { tags: 'comics' }],
-        },
-        orderBy: { createdAt: 'desc' },
-        limit: maxItems,
-      },
-    });
+    const { data } = await supabase
+      .from('files')
+      .select(selectFiles)
+      .in('tags', ['realistic', 'manga', 'anime', 'comics'])
+      .order('createdAt', { ascending: false })
+      .limit(maxItems);
 
-    for (const draw of drawings) {
-      const { fileId, name, shortDescription, pseudonym, profilePhoto, authorId, createdAt, updatedAt } = draw;
+    if (data?.length === 0) return filesArray;
+
+    for (const draw of data!) {
+      const { fileId, name, shortDescription, Users, authorId, createdAt, updatedAt } = draw;
 
       filesArray.push({
         fileId,
         name,
         shortDescription,
-        pseudonym,
-        profilePhoto,
+        pseudonym: Users[0].pseudonym!,
+        profilePhoto: `https://${cloudFrontUrl}/${Users[0].profilePhoto!}`,
         fileUrl: `https://${cloudFrontUrl}/${name}`,
         authorId,
         time: getDate(locale!, updatedAt! || createdAt!, dataDateObject),
@@ -55,126 +54,146 @@ async function getTop10Drawings(maxItems: string, locale: string, dataDateObject
 
     return filesArray;
   } catch (e) {
-    console.log('e', e);
+    console.error('10drawingsE', e);
   }
 }
 
-async function getTop10Photos(maxItems: string, locale: string, dataDateObject: DataDateObjectType) {
-  const filesArray: FileType[] = [];
+async function getTop10Photos(maxItems: number, locale: LangType, dataDateObject: DateObjectType) {
+  try {
+    const filesArray: FileType[] = [];
 
-  const photographs: { data: FileType[] } = await get(`${backUrl}/files/all`, {
-    queryData: {
-      where: { tags: 'photographs' },
-      orderBy: { createdAt: 'desc' },
-      limit: maxItems,
-    },
-  });
+    const { data } = await supabase
+      .from('files')
+      .select(selectFiles)
+      .eq('tags', 'photographs')
+      .order('createdAt', { ascending: false })
+      .limit(maxItems);
 
-  for (const photo of photographs.data) {
-    const { fileId, name, shortDescription, pseudonym, profilePhoto, authorId, createdAt, updatedAt } = photo;
+    if (data?.length === 0) return filesArray;
 
-    filesArray.push({
-      fileId,
-      name,
-      shortDescription,
-      pseudonym,
-      profilePhoto,
-      fileUrl: `https://${cloudFrontUrl}/${name}`,
-      authorId,
-      time: getDate(locale!, updatedAt! || createdAt!, dataDateObject),
-    });
+    for (const photo of data!) {
+      const { fileId, name, shortDescription, Users, authorId, createdAt, updatedAt } = photo;
+
+      filesArray.push({
+        fileId,
+        name,
+        shortDescription,
+        pseudonym: Users[0].pseudonym!,
+        profilePhoto: `https://${cloudFrontUrl}/${Users[0].profilePhoto!}`,
+        fileUrl: `https://${cloudFrontUrl}/${name}`,
+        authorId,
+        time: getDate(locale!, updatedAt! || createdAt!, dataDateObject),
+      });
+    }
+
+    return filesArray;
+  } catch (e) {
+    console.error('10photosE', e);
   }
-
-  return filesArray;
 }
 
-async function getTop10Animations(maxItems: string, locale: string, dataDateObject: DataDateObjectType) {
-  const filesArray: FileType[] = [];
+async function getTop10Animations(maxItems: number, locale: LangType, dataDateObject: DateObjectType) {
+  try {
+    const filesArray: FileType[] = [];
 
-  const animations: { data: FileType[] } = await get(`${backUrl}/files/all`, {
-    queryData: {
-      where: { tags: 'animations' },
-      orderBy: { createdAt: 'desc' },
-      limit: maxItems,
-    },
-  });
+    const { data } = await supabase
+      .from('files')
+      .select(selectFiles)
+      .eq('tags', 'animations')
+      .order('createdAt', { ascending: false })
+      .limit(maxItems);
 
-  for (const animation of animations.data) {
-    const { fileId, name, shortDescription, pseudonym, profilePhoto, authorId, createdAt, updatedAt } = animation;
+    if (data?.length === 0) return filesArray;
 
-    filesArray.push({
-      fileId,
-      name,
-      shortDescription,
-      pseudonym,
-      profilePhoto,
-      fileUrl: `https://${cloudFrontUrl}/${name}`,
-      authorId,
-      time: getDate(locale!, updatedAt! || createdAt!, dataDateObject),
-    });
+    for (const animation of data!) {
+      const { fileId, name, shortDescription, Users, authorId, createdAt, updatedAt } = animation;
+
+      filesArray.push({
+        fileId,
+        name,
+        shortDescription,
+        pseudonym: Users[0].pseudonym!,
+        profilePhoto: `https://${cloudFrontUrl}/${Users[0].profilePhoto!}`,
+        fileUrl: `https://${cloudFrontUrl}/${name}`,
+        authorId,
+        time: getDate(locale!, updatedAt! || createdAt!, dataDateObject),
+      });
+    }
+    return filesArray;
+  } catch (e) {
+    console.error('10animationsE', e);
   }
-  return filesArray;
 }
 
-async function getTop10Videos(maxItems: string, locale: string, dataDateObject: DataDateObjectType) {
-  const filesArray: FileType[] = [];
+async function getTop10Videos(maxItems: number, locale: LangType, dataDateObject: DateObjectType) {
+  try {
+    const filesArray: FileType[] = [];
 
-  const videos: { data: FileType[] } = await get(`${backUrl}/files/all`, {
-    queryData: {
-      where: { tags: 'videos' },
-      orderBy: { createdAt: 'desc' },
-      limit: maxItems,
-    },
-  });
+    const { data } = await supabase
+      .from('files')
+      .select(selectFiles)
+      .eq('tags', 'videos')
+      .order('createdAt', { ascending: false })
+      .limit(maxItems);
 
-  for (const video of videos.data) {
-    const { fileId, name, shortDescription, pseudonym, profilePhoto, authorId, createdAt, updatedAt } = video;
+    if (data?.length === 0) return filesArray;
 
-    filesArray.push({
-      fileId,
-      name,
-      shortDescription,
-      pseudonym,
-      profilePhoto,
-      fileUrl: `https://${cloudFrontUrl}/${name}`,
-      authorId,
-      time: getDate(locale!, updatedAt! || createdAt!, dataDateObject),
-    });
+    for (const video of data!) {
+      const { fileId, name, shortDescription, Users, authorId, createdAt, updatedAt } = video;
+
+      filesArray.push({
+        fileId,
+        name,
+        shortDescription,
+        pseudonym: Users[0].pseudonym!,
+        profilePhoto: `https://${cloudFrontUrl}/${Users[0].profilePhoto!}`,
+        fileUrl: `https://${cloudFrontUrl}/${name}`,
+        authorId,
+        time: getDate(locale!, updatedAt! || createdAt!, dataDateObject),
+      });
+    }
+
+    return filesArray;
+  } catch (e) {
+    console.error('10videoE', e);
   }
-
-  return filesArray;
 }
 
-async function getTop10Others(maxItems: string, locale: string, dataDateObject: DataDateObjectType) {
-  const filesArray: FileType[] = [];
+async function getTop10Others(maxItems: number, locale: LangType, dataDateObject: DateObjectType) {
+  try {
+    const filesArray: FileType[] = [];
 
-  const others: { data: FileType[] } = await get(`${backUrl}/files/all`, {
-    queryData: {
-      where: { tags: 'others' },
-      orderBy: { createdAt: 'desc' },
-      limit: maxItems,
-    },
-  });
+    const { data } = await supabase
+      .from('files')
+      .select(selectFiles)
+      .eq('tags', 'others')
+      .order('createdAt', { ascending: false })
+      .limit(maxItems);
 
-  for (const other of others.data) {
-    const { fileId, name, shortDescription, pseudonym, profilePhoto, authorId, createdAt, updatedAt } = other;
+    if (data?.length === 0) return filesArray;
 
-    filesArray.push({
-      fileId,
-      name,
-      shortDescription,
-      pseudonym,
-      profilePhoto,
-      fileUrl: `https://${cloudFrontUrl}/${name}`,
-      authorId,
-      time: getDate(locale!, updatedAt! || createdAt!, dataDateObject),
-    });
+    for (const other of data!) {
+      const { fileId, name, shortDescription, Users, authorId, createdAt, updatedAt } = other;
+
+      filesArray.push({
+        fileId,
+        name,
+        shortDescription,
+        pseudonym: Users[0].pseudonym!,
+        profilePhoto: `https://${cloudFrontUrl}/${Users[0].profilePhoto!}`,
+        fileUrl: `https://${cloudFrontUrl}/${name}`,
+        authorId,
+        time: getDate(locale!, updatedAt! || createdAt!, dataDateObject),
+      });
+    }
+
+    return filesArray;
+  } catch (e) {
+    console.error('10othersE', e);
   }
-
-  return filesArray;
 }
 
-export default async function App({ params: { locale } }: { params: { locale: string } }) {
+export default async function App({ params: { locale } }: { params: { locale: LangType } }) {
   setStaticParamsLocale(locale);
 
   const tApp = await getScopedI18n('App');
@@ -182,42 +201,34 @@ export default async function App({ params: { locale } }: { params: { locale: st
 
   const dataDateObject = await dateData();
 
-  const maxItems = '10';
+  const maxItems = 10;
 
-  const downloadDrawings = await getTop10Drawings(maxItems, locale, dataDateObject);
+  const drawings = await getTop10Drawings(maxItems, locale, dataDateObject);
 
-  const downloadPhotos = await getTop10Photos(maxItems, locale, dataDateObject);
+  const photos = await getTop10Photos(maxItems, locale, dataDateObject);
 
   const downloadAnimations = await getTop10Animations(maxItems, locale, dataDateObject);
 
-  const downloadVideos = await getTop10Videos(maxItems, locale, dataDateObject);
+  const videos = await getTop10Videos(maxItems, locale, dataDateObject);
 
-  const downloadOthers = await getTop10Others(maxItems, locale, dataDateObject);
+  const others = await getTop10Others(maxItems, locale, dataDateObject);
 
   return (
     <>
       <h2 className={styles.top__among__users}>{tApp('lastDrawings')}</h2>
       <AppWrapper>
-        {downloadDrawings!.length > 0 ? (
-          <AppTop10s data={downloadDrawings!} type="other" />
-        ) : (
-          <ZeroFiles text={tZero('drawings')} />
-        )}
+        {drawings!.length > 0 ? <AppTop10s data={drawings!} type="other" /> : <ZeroFiles text={tZero('drawings')} />}
       </AppWrapper>
 
       <h2 className={styles.top__among__users}>{tApp('lastPhotos')}</h2>
       <AppWrapper>
-        {downloadPhotos.length > 0 ? (
-          <AppTop10s data={downloadPhotos} type="other" />
-        ) : (
-          <ZeroFiles text={tZero('photos')} />
-        )}
+        {photos!.length > 0 ? <AppTop10s data={photos!} type="other" /> : <ZeroFiles text={tZero('photos')} />}
       </AppWrapper>
 
       <h2 className={styles.top__among__users}>{tApp('lastAnimations')}</h2>
       <AppWrapper>
-        {downloadAnimations.length > 0 ? (
-          <AppTop10s data={downloadAnimations} type="other" />
+        {downloadAnimations!.length > 0 ? (
+          <AppTop10s data={downloadAnimations!} type="other" />
         ) : (
           <ZeroFiles text={tZero('animations')} />
         )}
@@ -225,20 +236,12 @@ export default async function App({ params: { locale } }: { params: { locale: st
 
       <h2 className={styles.liked}>{tApp('lastVideos')}</h2>
       <AppWrapper>
-        {downloadVideos.length > 0 ? (
-          <AppTop10s data={downloadVideos} type="videos" />
-        ) : (
-          <ZeroFiles text={tZero('videos')} />
-        )}
+        {videos!.length > 0 ? <AppTop10s data={videos!} type="videos" /> : <ZeroFiles text={tZero('videos')} />}
       </AppWrapper>
 
       <h2 className={styles.top__among__users}>{tApp('lastOthers')}</h2>
       <AppWrapper>
-        {downloadOthers.length > 0 ? (
-          <AppTop10s data={downloadOthers} type="other" />
-        ) : (
-          <ZeroFiles text={tZero('others')} />
-        )}
+        {others!.length > 0 ? <AppTop10s data={others!} type="other" /> : <ZeroFiles text={tZero('others')} />}
       </AppWrapper>
     </>
   );
