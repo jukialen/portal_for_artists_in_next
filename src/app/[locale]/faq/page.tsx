101069e9f417b20417d8091000d0ceb61927793a
jukialen <pbidny@gmail.com>
2025-01-08 13:37:06 +0100

    Update: Faq page

--- src/app/[locale]/faq/page.tsx
+++ src/app/[locale]/faq/page.tsx
@@ -1,50 +1,52 @@
+import { ReactNode } from 'react';
 import { Metadata } from 'next';
 import Link from 'next/link';
 import { setStaticParamsLocale } from 'next-international/server';
 
-import { FaqItems } from 'components/atoms/FaqItems/FaqItems';
-import { AccordionWrapper } from 'components/atoms/AccordionWrapper/AccordionWrapper';
-
 import { getI18n, getScopedI18n } from 'locales/server';
 
 import { HeadCom } from 'constants/HeadCom';
+import { LangType } from 'types/global.types';
+
+import { AccordionWrapper } from 'components/atoms/AccordionWrapper/AccordionWrapper';
 
 import styles from './page.module.scss';
 
+
 export const metadata: Metadata = HeadCom('Faq site');
 
-export default async function Faq({ params: { locale } }: { params: { locale: string } }) {
+export default async function Faq({ params: { locale } }: { params: { locale: LangType } }) {
   setStaticParamsLocale(locale);
 
   const link = '#4F8DFF !important';
 
   const t = await getI18n();
   const tFAQ = await getScopedI18n('FAQ');
+
+  const items: { value: '' | number; head: string; body: string | ReactNode }[] = [
+    { value: 1, head: tFAQ('head1'), body: tFAQ('body1') },
+    {
+      value: 2,
+      head: tFAQ('head2'),
+      body: (
+        <div>
+          {tFAQ('body2')}
+          <Link href={`${locale}/plans`} color={link}>
+            {tFAQ('body2Link')}
+          </Link>
+          {tFAQ('body2dot')}
+        </div>
+      ),
+    },
+    { value: 3, head: tFAQ('head3'), body: tFAQ('body3') },
+    { value: 4, head: tFAQ('head4'), body: tFAQ('body4') },
+  ];
   
   return (
     <div className={styles.site}>
       <div className={styles.container}>
         <h2 className={styles.h2}>{t('Contact.toFAQHere')}</h2>
-        <AccordionWrapper>
-          <FaqItems textHead={tFAQ('head1')} textBody={tFAQ('body1')} />
-
-          <FaqItems
-            textHead={tFAQ('head2')}
-            textBody={
-              <div>
-                {tFAQ('body2')}
-                <Link href={`${locale}/plans`} color={link}>
-                  {tFAQ('body2Link')}
-                </Link>
-                {tFAQ('body2dot')}
-              </div>
-            }
-          />
-
-          <FaqItems textHead={tFAQ('head3')} textBody={tFAQ('body3')} />
-
-          <FaqItems textHead={tFAQ('head4')} textBody={tFAQ('body4')} />
-        </AccordionWrapper>
+        <AccordionWrapper items={items} />
       </div>
     </div>
   );
--- src/components/atoms/AccordionWrapper/AccordionWrapper.tsx
+++ src/components/atoms/AccordionWrapper/AccordionWrapper.tsx
@@ -1,19 +1,49 @@
 'use client';
 
-import { ReactNode } from 'react';
-import { Accordion } from '@chakra-ui/react';
+import { ReactNode, SetStateAction, useContext, useState } from 'react';
+import { AccordionItem, AccordionItemContent, AccordionItemTrigger, AccordionRoot } from 'components/ui/accordion';
+import { ModeContext } from 'providers/ModeProvider';
 
-export const AccordionWrapper = ({ children }: { children: ReactNode }) => {
+import { darkMode } from 'constants/links';
+
+import styles from './AccordionWrapper.module.scss';
+
+export const AccordionWrapper = ({
+  items,
+}: {
+  items: { value: '' | number; head: string; body: string | ReactNode }[];
+}) => {
+  const [value, setValue] = useState(['']);
+  const { isMode } = useContext(ModeContext);
+
+  const backgroundColor = 'transparent';
+  const color = isMode === darkMode ? '#f7f7f7' : '#2D3748';
+  const m = '2rem 0';
+
   return (
-    <Accordion
-      defaultIndex={[0]}
-      allowMultiple
+    <AccordionRoot
+      collapsible
+      multiple
+      unmountOnExit
+      defaultValue={['']}
+      value={value}
+      onValueChange={(e: { value: { toString: () => SetStateAction<string[]> } }) => setValue(e.value.toString())}
       width="95vw"
       maxW="26rem"
       justifyContent="space-around"
       alignSelf="center"
-      m="2rem 0">
-      {children}
-    </Accordion>
+      m>
+      <AccordionItem margin={m} backgroundColor={backgroundColor} color={color}>
+        <AccordionItemTrigger />
+        {items.map((item, index) => (
+          <AccordionItem key={index} value={item.value}>
+            <h2>
+              <AccordionItemTrigger className={styles.accordionButton}>{item.head}</AccordionItemTrigger>
+              <AccordionItemContent color={color}>{item.body}</AccordionItemContent>
+            </h2>
+          </AccordionItem>
+        ))}
+      </AccordionItem>
+    </AccordionRoot>
   );
 };
