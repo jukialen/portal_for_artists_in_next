8e508a44e8b786aca4e7b0609c7a70455f007d4d
jukialen <pbidny@gmail.com>
2025-01-16 14:03:53 +0100

    new links

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,5 +1,8 @@
 export const cloudFrontUrl = `${process.env.NEXT_PUBLIC_S3_URL}`;
 export const backUrl = `${process.env.NEXT_PUBLIC_PAGE}`;
+export const projectId =`${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`;
+export const access_token =`${process.env.NEXT_PUBLIC_SUPABASE_ACCESS_TOKEN}`;
+export const bucketName =`${process.env.NEXT_PUBLIC_SUPABASE_ACCESS_TOKEN}`;
 
 export const GTM_ID = process.env.NEXT_PUBLIC_G_TAG;
 
