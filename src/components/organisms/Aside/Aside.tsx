e60312591a628e2135279cb93ac339a3a25d69ac
jukialen <pbidny@gmail.com>
2025-01-22 13:54:28 +0100

    Update Aside.tsx

--- src/components/organisms/Aside/Aside.tsx
+++ src/components/organisms/Aside/Aside.tsx
@@ -1,9 +1,8 @@
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
-import { cloudFrontUrl } from 'constants/links';
 import { Database } from 'types/database.types';
-import { FriendsListArrayType, GroupsType } from "types/global.types";
+import { FriendsListArrayType, GroupsType } from 'types/global.types';
 
 import { getScopedI18n } from 'locales/server';
 import { getUserData } from 'helpers/getUserData';
@@ -14,23 +13,21 @@ const supabase = createServerComponentClient<Database>({ cookies });
 
 async function getFriendsList(userId: string, maxItems: number) {
   const favoriteFriendArray: FriendsListArrayType[] = [];
-  
+
   const { data, error } = await supabase
-  .from('Friends')
-  .select('Users (pseudonym, profilePhoto)')
-  .eq('usernameId', userId)
-  .eq('favorite', true)
-  .order('createdAt', { ascending: true })
-  .limit(maxItems);
-  
+    .from('Friends')
+    .select('Users (pseudonym, profilePhoto)')
+    .eq('usernameId', userId)
+    .eq('favorite', true)
+    .order('createdAt', { ascending: true })
+    .limit(maxItems);
+
   if (!data || !!error) return favoriteFriendArray;
-  
+
   for (const _f of data!) {
     favoriteFriendArray.push({
       pseudonym: _f.Users?.pseudonym!,
-      profilePhoto: !!_f.Users?.profilePhoto
-        ? `https://${cloudFrontUrl}/${_f.Users?.profilePhoto}`
-        : `${process.env.NEXT_PUBLIC_PAGE}/friends.svg`,
+      profilePhoto: !!_f.Users?.profilePhoto ? _f.Users?.profilePhoto : `${process.env.NEXT_PUBLIC_PAGE}/friends.svg`,
       favorites: data.length,
     });
   }
@@ -39,15 +36,15 @@ async function getFriendsList(userId: string, maxItems: number) {
 async function getGroupsList(maxItems: number) {
   const groupList: GroupsType[] = [];
   const user = await getUserData();
-  
+
   const { data, error } = await supabase
-  .from('UsersGroups')
-  .select('name, Groups (description, logo)')
-  .eq('userId', user?.id!)
-  .eq('favorite', true)
-  .order('createdAt', { ascending: true })
-  .limit(maxItems);
-  
+    .from('UsersGroups')
+    .select('name, Groups (description, logo)')
+    .eq('userId', user?.id!)
+    .eq('favorite', true)
+    .order('createdAt', { ascending: true })
+    .limit(maxItems);
+
   try {
     if (!data || !!error) return groupList;
 
@@ -55,7 +52,9 @@ async function getGroupsList(maxItems: number) {
       groupList.push({
         name: d.name,
         description: d?.Groups?.description!,
-        logo: !!d.Groups?.logo ? `https://${cloudFrontUrl}/${d.Groups?.logo}` : `${process.env.NEXT_PUBLIC_PAGE}/group.svg`,
+        logo: !!d.Groups?.logo
+          ? d.Groups?.logo
+          : `${process.env.NEXT_PUBLIC_PAGE}/group.svg`,
       });
     }
   } catch (e) {
@@ -67,10 +66,10 @@ export async function Aside() {
   const tAside = await getScopedI18n('Aside');
   const userData = await getUserData();
   const maxItems = 5;
-  
+
   const friendsAsideList = await getFriendsList(userData?.id!, maxItems);
   const groupsAsideList = await getGroupsList(maxItems);
-  
+
   return (
     <AsideWrapper
       asideCategory={tAside('category')}
