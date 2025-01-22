5b4fa2ed7e27fc9712c6c7317ffd5306d24f5fc1
jukialen <pbidny@gmail.com>
2025-01-22 13:51:54 +0100

    Update FormForgotten.tsx

--- src/components/molecules/FormForgotten/FormForgotten.tsx
+++ src/components/molecules/FormForgotten/FormForgotten.tsx
@@ -5,7 +5,7 @@ import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { Form, Formik } from 'formik';
 import * as Yup from 'yup';
 import { SchemaValidation } from 'shemasValidation/schemaValidation';
-import { Divider, Input } from '@chakra-ui/react';
+import { Separator, Input } from '@chakra-ui/react';
 
 import { LangType, ResetFormType, UserFormType } from 'types/global.types';
 
@@ -31,21 +31,19 @@ export const FormForgotten = ({ locale }: { locale: LangType }) => {
     email: SchemaValidation().email,
   });
 
-  console.log("t('Forgotten.success')", t('Forgotten.success'));
   const reset__password = async ({ email }: UserFormType, { resetForm }: ResetFormType) => {
     try {
-      const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+      const { error } = await supabase.auth.resetPasswordForEmail(email, {
         redirectTo: `${process.env.NEXT_PUBLIC_PAGE}/${locale}/new-password`,
       });
 
-      console.log('resetEmailData', data);
-      console.log('resetEmailEr', error);
       if (error?.status === 200) {
         setValuesFields(error?.message!);
+        return;
       }
       resetForm(initialValues);
       setValuesFields(t('Forgotten.success'));
-    } catch (e: any) {
+    } catch (e) {
       console.error(e);
       setValuesFields(t('unknownError'));
       setValuesFields(t('error'));
@@ -58,7 +56,7 @@ export const FormForgotten = ({ locale }: { locale: LangType }) => {
         <Form className={styles.forgotten}>
           <div className={styles.borderContainer}>
             <h2 className={styles.title}>{t('Forgotten.title')}</h2>
-            <Divider />
+            <Separator />
             <h3 className={styles.subtitle}>{t('Forgotten.subtitle')}</h3>
             <Input
               name="email"
