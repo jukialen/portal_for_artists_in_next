74093bf9bff90bf9963f2b41f89571c7dc7812ca
jukialen <piteru1910@yahoo.com>
2025-07-27 12:36:21 +0200

    Improvement: deletion of 1  image pattern for next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 /** @type {import("next").NextConfig} */
 
-const S3_HOST = process.env.NEXT_PUBLIC_S3_URL;
-
 module.exports = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: S3_HOST,
-        port: '',
-        pathname: '/**',
-      },
       {
         protocol: 'https',
         hostname: 's.yimg.com',
