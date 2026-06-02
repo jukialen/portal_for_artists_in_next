a125cf4d86ccda27b14350e27e2713f4f4c51f13
Piotr Bidny <piteru1910@yahoo.com>
2026-06-02 15:13:22 +0200

    improvement: simplify of next.config.ts

--- next.config.js
+++ next.config.js
@@ -94,20 +94,7 @@ const nextConfig = {
         ],
       },
       {
-        source: '/sw.js',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/javascript; charset=utf-8',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'no-cache, no-store, must-revalidate',
-          },
-        ],
-      },
-      {
-        source: '/workbox-:hash.js',
+        source: '/(sw.js|workbox-.*.js)',
         headers: [
           {
             key: 'Content-Type',
